f7b51e66cb47444b133afa7ad94e8133d7ca2088
Christopher Giroir <kelsin@valefor.com>
2011-01-22 08:47:22 -0500

    Version 0.1

--- cod-officer.lua
+++ cod-officer.lua
@@ -1,35 +1,56 @@
-function CoD:RepairOn
-   for i = 1, GetNumRaidMembers() do
-      r_name, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil = GetRaidRosterInfo(i);
-      if GetGuildInfo(r_name) == "Cake or Death" then
-         for j = 1, GetNumGuildMembers() do
-            g_name, g_rank, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil = GetGuildRosterInfo(j);
-            if g_name == r_name and g_rank == "Member" then
-               GuildPromote(g_name);
-            end
-         end
+CoD = {};
+
+function CoD:OnLoad()
+   print("CoD Officer Mod Loaded");
+end
+
+function CoD:RepairOn()
+   print("Promoting current raid members to Raider");
+
+   local guild, _, _ = GetGuildInfo("player");
+
+   for i=0, GetNumRaidMembers() do
+      local name, _, _, _, _, _, _, _, _, _, _ = GetRaidRosterInfo(i);
+      
+      local guildName, guildRankName, _ = GetGuildInfo("raid" .. i);
+         
+      if guildRankName == "Member" and guildName == guild then
+         GuildPromote(name);
       end
    end
 end
 
-function CoD:RepairOff
-   for j = 1, GetNumGuildMembers() do
-      g_name, g_rank, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil = GetGuildRosterInfo(j);
-      if (g_rank == "Raider") then
-         GuildDemote(g_name);
+function CoD:RepairOff()
+   print("Demoting all Raiders");
+
+   local guild, _, _ = GetGuildInfo("player");
+
+   for i=0, GetNumRaidMembers() do
+      local name, _, _, _, _, _, _, _, _, _, _ = GetRaidRosterInfo(i);
+      
+      local guildName, guildRankName, _ = GetGuildInfo("raid" .. i);
+         
+      if guildRankName == "Raider" and guildName == guild then
+         GuildDemote(name);
       end
    end
 end
 
--- Slash command
-SLASH_COD1 = '/cod'
-
-function SlashCmdList.COD(msg, editbox)
+function CoD:Handler(msg, editbox)
    local command, rest = msg:match("^(%S*)%s*(.-)$");
 
-   if command == "repair" and rest == "on" then
-      CoD:RepairOn()
-   elseif command == "repair" and rest == "off" then
-      CoD:RepairOff()
+   if command == "repair" then
+      if rest == "on" then
+         CoD:RepairOn();
+      elseif rest == "off" then
+         CoD:RepairOff();
+      else
+	 print("Syntax: /cod repair on|off");
+      end
+   else
+      print("Commands: repair");
    end
 end
+
+SLASH_COD1 = "/cod";
+SlashCmdList["COD"] = function(msg, editbox) CoD:Handler(msg, editbox) end
